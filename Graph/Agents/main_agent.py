7c02a9d42fbde5f08fcf94361591a4f97aaedc1b
RishabhG2187 <rishabhg2187@gmail.com>
2025-07-07 11:11:27 +0530

    Chatbot graph

--- Graph/Agents/main_agent.py
+++
@@ -1,4 +0,0 @@
-class MainAgent:
-    def run(self, state):
-        print("Initializing LangGraph and global config...")
-        return state
--- Graph/Agents/stt_node.py
+++
@@ -1,6 +0,0 @@
-class SpeechToTextAgent:
-    def run(self, state):
-        print("Mocking STT for now...")
-        # In reality, you'd convert audio input to text here.
-        state["transcribed_text"] = "My name is John, I am 42, I have chest pain and diabetes."
-        return state
